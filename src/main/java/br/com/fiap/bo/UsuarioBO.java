8715d85e24760795b8e9074e40ce12f7382efa26
vitordias1006 <vitordias09532@gmail.com>
2025-11-20 16:54:13 -0300

    ghij

--- src/main/java/br/com/fiap/bo/UsuarioBO.java
+++ src/main/java/br/com/fiap/bo/UsuarioBO.java
@@ -1,44 +1,49 @@
-package br.com.fiap.bo;
+    package br.com.fiap.bo;
 
-import br.com.fiap.dao.UsuarioDAO;
-import br.com.fiap.to.UsuarioTO;
+    import br.com.fiap.dao.UsuarioDAO;
+    import br.com.fiap.to.UsuarioTO;
 
-public class UsuarioBO {
-    private UsuarioDAO usuarioDAO;
+    public class UsuarioBO {
+        private UsuarioDAO usuarioDAO;
 
-    public UsuarioTO saveUser(UsuarioTO usuarioTO) {
-        usuarioDAO = new UsuarioDAO();
+        public UsuarioTO saveUser(UsuarioTO usuarioTO) {
+            usuarioDAO = new UsuarioDAO();
 
-        UsuarioTO emailExiste = usuarioDAO.findByEmail(usuarioTO.getEmail());
-        if (emailExiste != null) {
-            throw new RuntimeException("Email já está em uso.");
+            UsuarioTO emailExiste = usuarioDAO.findByEmail(usuarioTO.getEmail());
+            if (emailExiste != null) {
+                throw new RuntimeException("Email já está em uso.");
+            }
+
+            return usuarioDAO.saveUser(usuarioTO);
         }
 
-        return usuarioDAO.saveUser(usuarioTO);
-    }
+        public UsuarioTO findById(Long id) {
+            usuarioDAO = new UsuarioDAO();
 
-    public UsuarioTO findById(Long id) {
-        usuarioDAO = new UsuarioDAO();
-
-        return usuarioDAO.findById(id);
-    }
-
-    public UsuarioTO updateUser(UsuarioTO usuarioTO) {
-        usuarioDAO = new UsuarioDAO();
-
-        return usuarioDAO.updateUser(usuarioTO);
-    }
-
-    public boolean deleteUser(Long id) {
-        usuarioDAO = new UsuarioDAO();
-
-        return usuarioDAO.deleteById(id);
-    }
-
-    public UsuarioTO findByEmailAndPassword(String email, String password) {
-        if(usuarioDAO == null){
-            throw new RuntimeException("Usuário não existente");
+            return usuarioDAO.findById(id);
         }
-        return usuarioDAO.findByEmailAndPassword(email, password);
+
+        public UsuarioTO updateUser(UsuarioTO usuarioTO) {
+            usuarioDAO = new UsuarioDAO();
+
+            return usuarioDAO.updateUser(usuarioTO);
+        }
+
+        public boolean deleteUser(Long id) {
+            usuarioDAO = new UsuarioDAO();
+
+            return usuarioDAO.deleteById(id);
+        }
+
+        public UsuarioTO findByEmailAndPassword(String email, String password) {
+            usuarioDAO = new UsuarioDAO();
+
+            UsuarioTO user = usuarioDAO.findByEmailAndPassword(email, password);
+
+            if (user == null) {
+                throw new RuntimeException("Usuário não existente");
+            }
+
+            return user;
+        }
     }
-}
